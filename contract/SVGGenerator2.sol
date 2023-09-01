a5decfa32650c507876118680cc764a401339724
aidatorajiro <kawarusosu@zoho.com>
2023-09-01 19:56:26 +0900

    second one

---
+++ contract/SVGGenerator2.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8;
+
+import "defs.sol";
+
+// 0x06ae046986A584514E343fe6E3494D15E713E37a
+
+contract SVGGeneratorDisp2 is ISVGGenerator {
+    function generate(NFTMeta memory meta)
+        public
+        view
+        returns (string memory)
+    {
+        return string(abi.encodePacked(
+'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500">',
+'    <filter id="image">',
+'        <feImage result="img1" href=""/>',
+'        <feImage result="img2" href=""/>',
+'        <feImage result="img3" href=""/>',
+'        <feImage result="img4" href=""/>',
+'        <feImage result="img5" href=""/>',
+'',
+'        <feDisplacementMap in="SourceGraphic" in2="img1" result="final" scale="2" xChannelSelector="R" yChannelSelector="G" />',
+'        <feComposite in="final" in2="img1" operator="arithmetic" result="final" k1="2" k2="1" k3="1" k4="0" />',
+'        <feComposite in="final" in2="img2" operator="arithmetic" result="final" k1="2" k2="1" k3="0.1" k4="0" />',
+'        <feComposite in="final" in2="img3" operator="arithmetic" result="final" k1="2" k2="1" k3="0.1" k4="0" />',
+'        <feComposite in="final" in2="img4" operator="arithmetic" result="final" k1="2" k2="1" k3="0.1" k4="0" />',
+'        <feComposite in="final" in2="img5" operator="arithmetic" result="final" k1="2" k2="1" k3="0.1" k4="0" />',
+'        <feComponentTransfer in="final" result="final">',
+'            <feFuncR type="gamma" amplitude="1" exponent="2" offset="0"></feFuncR>',
+'            <feFuncG type="gamma" amplitude="1" exponent="5" offset="0"></feFuncG>',
+'            <feFuncB type="gamma" amplitude="1" exponent="2" offset="0"></feFuncB>',
+'        </feComponentTransfer>',
+'    </filter>',
+'    <text x="0" y="8" font-family="Verdana" font-size="12" transform="scale(6 50)" style="filter:url(#image);fill:#F89723;" >', meta.base_serial,'</text>',
+'</svg>'
+));
+    }
+}
